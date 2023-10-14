88289405dbf1dfd9d7762331f2a2e311ae629cf4
elinarite <elina.arite@gmail.com>
2023-10-14 17:11:26 +0200

    add bot prototype with test

--- src/test/java/com/example/notificationservice/NotificationServiceApplicationTest.java
+++ src/test/java/com/example/notificationservice/NotificationServiceApplicationTest.java
@@ -1,3 +1,5 @@
+package com.example.notificationservice;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
