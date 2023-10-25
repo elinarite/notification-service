2e56f07290157700eb83b4446a85228d0961dc65
elinarite <elina.arite@gmail.com>
2023-10-25 21:41:07 +0200

    test testcontainers

--- src/test/java/com/example/notification/NotificationServiceApplicationTest.java
+++ src/test/java/com/example/notification/NotificationServiceApplicationTest.java
@@ -3,41 +3,17 @@ package com.example.notification;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-
-@Testcontainers
+//@Testcontainers
 @SpringBootTest(classes = NotificationServiceApplicationTest.class)
 class NotificationServiceApplicationTest {
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13");
-
-//    @DynamicPropertySource
-//    static void postgresSqlProperties(DynamicPropertyRegistry registry){
-//        registry.add("postgres.driver", postgreSQLContainer::getDriverClassName);
-//    }
-//
-//    try (Connection conn = DriverManager.getConnection(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword());
-//    Statement stmt = conn.createStatement()) {
-//        stmt.execute("CREATE SCHEMA notification_bot");
-//    }
-//
-//    NotificationServiceApplicationTest() throws SQLException {
-//    }
+//    @Container
+//    @ServiceConnection
+//    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13");
 
     @Test
     void contextLoads() {
