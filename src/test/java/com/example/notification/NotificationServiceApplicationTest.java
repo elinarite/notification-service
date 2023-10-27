4aa6ccc1bd1ee959c4d72c68ae9ed47a756cccb8
elinarite <elina.arite@gmail.com>
2023-10-27 17:18:46 +0200

    add testcontainers

--- src/test/java/com/example/notification/NotificationServiceApplicationTest.java
+++ src/test/java/com/example/notification/NotificationServiceApplicationTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest
+@ActiveProfiles("test")
 class NotificationServiceApplicationTest extends BaseTest{
 
     @Container
