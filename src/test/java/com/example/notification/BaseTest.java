a6e51d71448995f0ac59a5ca8e69c485cddb4401
elinarite <elina.arite@gmail.com>
2023-10-27 18:40:16 +0200

    add testcontainers

--- src/test/java/com/example/notification/BaseTest.java
+++ src/test/java/com/example/notification/BaseTest.java
@@ -3,10 +3,14 @@ package com.example.notification;
 import com.example.notification.service.impl.TelegramBotService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @SpringBootTest
 public class BaseTest {
 
     @MockBean
     public TelegramBotService telegramBotService;
+
+    @MockBean
+    public TelegramBotsApi telegramBotsApi;
 }
