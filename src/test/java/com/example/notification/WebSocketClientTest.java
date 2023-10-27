29918252ffe569ee21d8a87efbcdd83850c2cfb1
elinarite <elina.arite@gmail.com>
2023-10-27 19:45:38 +0200

    add testcontainers

--- src/test/java/com/example/notification/WebSocketClientTest.java
+++ src/test/java/com/example/notification/WebSocketClientTest.java
@@ -11,33 +11,33 @@ import steps.WebSocketSteps;
 
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@ActiveProfiles("test")
-public class WebSocketClientTest extends BaseTest {
-    private WebSocketSteps webSocketSteps;
-    private SubscriptionSteps subscriptionSteps;
-
-    @BeforeEach
-    public void setup() {
-        webSocketSteps = new WebSocketSteps();
-        subscriptionSteps = new SubscriptionSteps();
-    }
-
-    @Test
-    public void testWebSocketIntegration() throws InterruptedException {
-        WebSocketListener listener = new WebSocketListener() {
-            @Override
-            public void onMessage(okhttp3.WebSocket webSocket, String text) {
+//@SpringBootTest
+//@ActiveProfiles("test")
+//public class WebSocketClientTest extends BaseTest {
+//    private WebSocketSteps webSocketSteps;
+//    private SubscriptionSteps subscriptionSteps;
+//
+//    @BeforeEach
+//    public void setup() {
+//        webSocketSteps = new WebSocketSteps();
+//        subscriptionSteps = new SubscriptionSteps();
+//    }
+//
+//    @Test
+//    public void testWebSocketIntegration() throws InterruptedException {
+//        WebSocketListener listener = new WebSocketListener() {
+//            @Override
+//            public void onMessage(okhttp3.WebSocket webSocket, String text) {
 //                telegramBotService.sendMessage(1361169404L, "New message: " + text);
-            }
-        };
-
-        WebSocket webSocket = webSocketSteps.connectToWebSocket(listener);
-
-        subscriptionSteps.sendSubscription(webSocket);
-
-        Thread.sleep(10000);
-
-        verify(telegramBotService, atLeastOnce()).sendMessage(anyLong(), startsWith("New message:"));
-    }
-}
+//            }
+//        };
+//
+//        WebSocket webSocket = webSocketSteps.connectToWebSocket(listener);
+//
+//        subscriptionSteps.sendSubscription(webSocket);
+//
+//        Thread.sleep(10000);
+//
+//        verify(telegramBotService, atLeastOnce()).sendMessage(anyLong(), startsWith("New message:"));
+//    }
+//}
