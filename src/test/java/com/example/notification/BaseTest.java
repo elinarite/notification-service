2d33a34015d5e30489ba42ca5c010debc7840ed2
elinarite <elina.arite@gmail.com>
2023-10-27 18:38:08 +0200

    add testcontainers

--- src/test/java/com/example/notification/BaseTest.java
+++ src/test/java/com/example/notification/BaseTest.java
@@ -1,23 +1,12 @@
 package com.example.notification;
 
 import com.example.notification.service.impl.TelegramBotService;
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @SpringBootTest
 public class BaseTest {
 
     @MockBean
     public TelegramBotService telegramBotService;
-
-//    @MockBean
-//    public TelegramBotsApi telegramBotsApi;
-//
-//    @BeforeEach
-//    public void setupBase() {
-//        Mockito.when(telegramBotService.someMethod()).thenReturn(expectedValue);
-//    }
 }
--- src/test/java/com/example/notification/NotificationServiceApplicationTest.java
+++ src/test/java/com/example/notification/NotificationServiceApplicationTest.java
@@ -1,35 +1,14 @@
 package com.example.notification;
 
-import com.example.notification.service.impl.TelegramBotService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 class NotificationServiceApplicationTest extends BaseTest{
-
-    @Container
-    @ServiceConnection
-    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13")
-            .withDatabaseName("testdb")
-            .withUsername("testuser")
-            .withPassword("testpassword");
-
-    @DynamicPropertySource
-    public static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-    }
 
     @Test
     void contextLoads() {
