a377f22749f9a2de3badc5867363e8364bd8feb9
elinarite <elina.arite@gmail.com>
2023-11-05 18:40:12 +0100

    add Mexc Api with tests

--- src/test/java/com/example/notification/api/MexcServiceJsonTest.java
+++ src/test/java/com/example/notification/api/MexcServiceJsonTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -13,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class MexcServiceJsonTest extends BaseTest {
 
     @MockBean
--- src/test/java/com/example/notification/api/MexcServiceTest.java
+++ src/test/java/com/example/notification/api/MexcServiceTest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Slf4j
+@ActiveProfiles("test")
 public class MexcServiceTest extends BaseTest {
 
     @Autowired
